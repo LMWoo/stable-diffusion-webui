18cb3d5d817e91ebe37164ccf9e04635edbe7ad4
LMWoo <leemw513@gmail.com>
2023-07-03 13:22:40 +0900

    add create embedding

--- modules/textual_inversion/ui.py
+++ modules/textual_inversion/ui.py
@@ -8,11 +8,42 @@ from modules import sd_hijack, shared
 
 
 def create_embedding(name, initialization_text, nvpt, overwrite_old):
-    filename = modules.textual_inversion.textual_inversion.create_embedding(name, nvpt, overwrite_old, init_text=initialization_text)
+    import requests
+    import json
+    import base64
+    from modules.api.models import PreprocessResponse
 
-    sd_hijack.model_hijack.embedding_db.load_textual_inversion_embeddings()
+    url = "http://mwgpu.mydomain.blog:4000/sdapi/v1/create/embedding"
 
-    return gr.Dropdown.update(choices=sorted(sd_hijack.model_hijack.embedding_db.word_embeddings.keys())), f"Created: {filename}", ""
+    payload = json.dumps({
+        "name": name,
+        "init_text": initialization_text,
+        "num_vectors_per_token": nvpt,
+        "overwrite_old": overwrite_old,
+    })
+
+    auth = 'user:password'
+    auth_bytes = auth.encode('UTF-8')
+
+    auth_encoded = base64.b64encode(auth_bytes)
+    auth_encoded = bytes(auth_encoded)
+    auth_encoded_str = auth_encoded.decode('UTF-8')
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + auth_encoded_str
+    }
+
+    response = requests.request("POST", url=url, headers=headers, data=payload)
+    print(response.json())
+
+
+    return None, f"Created embedding", ""
+    # filename = modules.textual_inversion.textual_inversion.create_embedding(name, nvpt, overwrite_old, init_text=initialization_text)
+
+    # sd_hijack.model_hijack.embedding_db.load_textual_inversion_embeddings()
+
+    # return gr.Dropdown.update(choices=sorted(sd_hijack.model_hijack.embedding_db.word_embeddings.keys())), f"Created: {filename}", ""
 
 
 def preprocess(*args):
