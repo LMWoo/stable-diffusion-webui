11c735eebcc439e2c81b1975e1e2e4100f875379
LMWoo <leemw513@gmail.com>
2023-07-04 19:40:34 +0900

    Select Textual Inversion

--- modules/ui_extra_networks_textual_inversion.py
+++ modules/ui_extra_networks_textual_inversion.py
@@ -10,20 +10,52 @@ class ExtraNetworksPageTextualInversion(ui_extra_networks.ExtraNetworksPage):
         self.allow_negative_prompt = True
 
     def refresh(self):
-        sd_hijack.model_hijack.embedding_db.load_textual_inversion_embeddings(force_reload=True)
+        from modules.textual_inversion.textual_inversion import Embedding
+        import base64
+        import requests
+        from modules.api.models import EmbeddingsResponse
+
+        url = "http://mwgpu.mydomain.blog:4000/sdapi/v1/embeddings"
+
+        auth = 'user:password'
+        auth_bytes = auth.encode('UTF-8')
+
+        auth_encoded = base64.b64encode(auth_bytes)
+        auth_encoded = bytes(auth_encoded)
+        auth_encoded_str = auth_encoded.decode('UTF-8')
+
+        headers = {
+            'accept': 'application/json',
+            'Authorization': 'Basic ' + auth_encoded_str,
+        }
+
+        response = requests.get(url=url, headers=headers)
+        res = EmbeddingsResponse(**(response.json()))
+
+        embeddings = {}
+        for key in res.loaded.keys():
+            newEmbedding = Embedding(None, key)
+            newEmbedding.sd_checkpoint = res.loaded[key].sd_checkpoint
+            newEmbedding.name = key
+            newEmbedding.filename = key
+            
+            embeddings[key] = newEmbedding
+            
+        sd_hijack.model_hijack.embedding_db.word_embeddings.update(embeddings)
+        # sd_hijack.model_hijack.embedding_db.load_textual_inversion_embeddings(force_reload=True)
 
     def list_items(self):
         for index, embedding in enumerate(sd_hijack.model_hijack.embedding_db.word_embeddings.values()):
-            path, ext = os.path.splitext(embedding.filename)
+            # path, ext = os.path.splitext(embedding.filename)
             yield {
                 "name": embedding.name,
-                "filename": embedding.filename,
-                "preview": self.find_preview(path),
-                "description": self.find_description(path),
-                "search_term": self.search_terms_from_path(embedding.filename),
+                "filename": embedding.name,
+                "preview": None,
+                "description": None,
+                "search_term": None,
                 "prompt": json.dumps(embedding.name),
-                "local_preview": f"{path}.preview.{shared.opts.samples_format}",
-                "sort_keys": {'default': index, **self.get_sort_keys(embedding.filename)},
+                "local_preview": None,
+                # "sort_keys": {'default': index, **self.get_sort_keys(embedding.filename)},
 
             }
 
