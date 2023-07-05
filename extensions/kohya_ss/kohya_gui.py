2943d1264100fb6aad95e3343d69b9c679b12de6
LMWoo <leemw513@gmail.com>
2023-07-05 13:51:36 +0900

    add tab kohya

--- extensions/kohya_ss/kohya_gui.py
+++ extensions/kohya_ss/kohya_gui.py
@@ -16,33 +16,34 @@ from lora_gui import lora_tab
 import os
 from library.custom_logging import setup_logging
 
+from modules import script_callbacks
+
 # Set up logging
 log = setup_logging()
 
-
-def UI(**kwargs):
+def UI():
+    import sys
+    import os
+    sys.path.append("/home/mwlee/stable-diffusion-webui/extensions/kohya_ss")
     css = ''
 
-    headless = kwargs.get('headless', False)
-    log.info(f'headless: {headless}')
-
-    if os.path.exists('./style.css'):
-        with open(os.path.join('./style.css'), 'r', encoding='utf8') as file:
-            log.info('Load CSS...')
-            css += file.read() + '\n'
+    headless = False #kwargs.get('headless', False)
+    # if os.path.exists('./style.css'):
+    #     with open(os.path.join('./style.css'), 'r', encoding='utf8') as file:
+    #         log.info('Load CSS...')
+    #         css += file.read() + '\n'
             
-    if os.path.exists('./.release'):
-        with open(os.path.join('./.release'), 'r', encoding='utf8') as file:
-            release= file.read()
-
-    interface = gr.Blocks(
-        css=css, title=f'Kohya_ss GUI {release}', theme=gr.themes.Default()
+    # if os.path.exists('./.release'):
+    #     with open(os.path.join('./.release'), 'r', encoding='utf8') as file:
+    #         releasefile = file.read()
+            
+    interface = gr.Blocks( title=f'Kohya_ss GUI', theme=gr.themes.Default()
     )
 
     with interface:
-        gr.Markdown(
-            f'kohya_ss GUI release {release}'
-        )
+        # gr.Markdown(
+        #     f'kohya_ss GUI release {releasefile}'
+        # )
         with gr.Tab('Dreambooth'):
             (
                 train_data_dir_input,
@@ -72,67 +73,69 @@ def UI(**kwargs):
                 gradio_merge_lora_tab(headless=headless)
                 gradio_merge_lycoris_tab(headless=headless)
                 gradio_resize_lora_tab(headless=headless)
+    
+    return [(interface, "Kohya", "kohya")]
 
     # Show the interface
-    launch_kwargs = {}
-    username = kwargs.get('username')
-    password = kwargs.get('password')
-    server_port = kwargs.get('server_port', 0)
-    inbrowser = kwargs.get('inbrowser', False)
-    share = kwargs.get('share', False)
-    server_name = kwargs.get('listen')
+    # launch_kwargs = {}
+    # username = kwargs.get('username')
+    # password = kwargs.get('password')
+    # server_port = kwargs.get('server_port', 0)
+    # inbrowser = kwargs.get('inbrowser', False)
+    # share = kwargs.get('share', False)
+    # server_name = kwargs.get('listen')
 
-    launch_kwargs['server_name'] = server_name
-    if username and password:
-        launch_kwargs['auth'] = (username, password)
-    if server_port > 0:
-        launch_kwargs['server_port'] = server_port
-    if inbrowser:
-        launch_kwargs['inbrowser'] = inbrowser
-    if share:
-        launch_kwargs['share'] = share
-    interface.launch(**launch_kwargs)
+    # launch_kwargs['server_name'] = server_name
+    # if username and password:
+    #     launch_kwargs['auth'] = (username, password)
+    # if server_port > 0:
+    #     launch_kwargs['server_port'] = server_port
+    # if inbrowser:
+    #     launch_kwargs['inbrowser'] = inbrowser
+    # if share:
+    #     launch_kwargs['share'] = share
+    # interface.launch(**launch_kwargs)
 
 
-if __name__ == '__main__':
-    # torch.cuda.set_per_process_memory_fraction(0.48)
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--listen',
-        type=str,
-        default='127.0.0.1',
-        help='IP to listen on for connections to Gradio',
-    )
-    parser.add_argument(
-        '--username', type=str, default='', help='Username for authentication'
-    )
-    parser.add_argument(
-        '--password', type=str, default='', help='Password for authentication'
-    )
-    parser.add_argument(
-        '--server_port',
-        type=int,
-        default=0,
-        help='Port to run the server listener on',
-    )
-    parser.add_argument(
-        '--inbrowser', action='store_true', help='Open in browser'
-    )
-    parser.add_argument(
-        '--share', action='store_true', help='Share the gradio UI'
-    )
-    parser.add_argument(
-        '--headless', action='store_true', help='Is the server headless'
-    )
+# if __name__ == '__main__':
+#     # torch.cuda.set_per_process_memory_fraction(0.48)
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument(
+#         '--listen',
+#         type=str,
+#         default='127.0.0.1',
+#         help='IP to listen on for connections to Gradio',
+#     )
+#     parser.add_argument(
+#         '--username', type=str, default='', help='Username for authentication'
+#     )
+#     parser.add_argument(
+#         '--password', type=str, default='', help='Password for authentication'
+#     )
+#     parser.add_argument(
+#         '--server_port',
+#         type=int,
+#         default=0,
+#         help='Port to run the server listener on',
+#     )
+#     parser.add_argument(
+#         '--inbrowser', action='store_true', help='Open in browser'
+#     )
+#     parser.add_argument(
+#         '--share', action='store_true', help='Share the gradio UI'
+#     )
+#     parser.add_argument(
+#         '--headless', action='store_true', help='Is the server headless'
+#     )
 
-    args = parser.parse_args()
+#     args = parser.parse_args()
 
-    UI(
-        username=args.username,
-        password=args.password,
-        inbrowser=args.inbrowser,
-        server_port=args.server_port,
-        share=args.share,
-        listen=args.listen,
-        headless=args.headless,
-    )
+#     UI(
+#         username=args.username,
+#         password=args.password,
+#         inbrowser=args.inbrowser,
+#         server_port=args.server_port,
+#         share=args.share,
+#         listen=args.listen,
+#         headless=args.headless,
+#     )
