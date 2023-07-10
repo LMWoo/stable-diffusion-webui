d820aadaa7889853746b0cfef58f025d96dc919d
LMWoo <leemw513@gmail.com>
2023-07-10 14:47:30 +0900

    Get Hypernetwork from api server

--- modules/ui_extra_networks_hypernets.py
+++ modules/ui_extra_networks_hypernets.py
@@ -9,6 +9,36 @@ class ExtraNetworksPageHypernetworks(ui_extra_networks.ExtraNetworksPage):
         super().__init__('Hypernetworks')
 
     def refresh(self):
+        from modules.hypernetworks.hypernetwork import Hypernetwork
+        import base64
+        import requests
+        from modules import sd_hijack
+        from modules.api.models import HypernetworkItem
+
+        url = "http://mwgpu.mydomain.blog:4000/sdapi/v1/hypernetworks"
+
+        auth = 'user:password'
+        auth_bytes = auth.encode('UTF-8')
+        
+        auth_encoded = base64.b64encode(auth_bytes)
+        auth_encoded = bytes(auth_encoded)
+        auth_encoded_str = auth_encoded.decode('UTF-8')
+
+        headers = {
+            'accept': 'application/json',
+            'Authorization': 'Basic ' + auth_encoded_str,
+        }
+
+        response = requests.get(url=url, headers=headers)
+        print(response.json())
+        items = response.json()
+        shared.hypernetworks.clear()
+        
+        for item in items:
+            newItem = Hypernetwork(name = item["name"])
+            shared.hypernetworks[item["name"]] = item["name"]
+        
+    def refresh_req(self):
         shared.reload_hypernetworks()
 
     def list_items(self):
@@ -23,7 +53,7 @@ class ExtraNetworksPageHypernetworks(ui_extra_networks.ExtraNetworksPage):
                 "search_term": self.search_terms_from_path(path),
                 "prompt": json.dumps(f"<hypernet:{name}:") + " + opts.extra_networks_default_multiplier + " + json.dumps(">"),
                 "local_preview": f"{path}.preview.{shared.opts.samples_format}",
-                "sort_keys": {'default': index, **self.get_sort_keys(path + ext)},
+                # "sort_keys": {'default': index, **self.get_sort_keys(path + ext)},
 
             }
 
